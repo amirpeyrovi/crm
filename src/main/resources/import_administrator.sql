339b455a103bc2672c4ef75bb632672a78719a66
shabnamarasteh <64069037+shabnamarasteh@users.noreply.github.com>
2020-07-09 06:23:13 -0800

    adminpermission script

--- src/main/resources/import_administrator.sql
+++ src/main/resources/import_administrator.sql
@@ -6,9 +6,9 @@ where not exists(select id from crm_admin where username = 'administrator');
 insert into crm_admin_role (title , id) select 'administrator' , role_seq.nextval from dual where not exists (select id from crm_admin_role where title = 'administrator');
 update crm_admin set admin_role_id = (select id from crm_admin_role where title ='administrator');
 
-insert into crm_admin_role_permission (adminRolePermissionId, ADMIN_ROLE_ID, PERMISSION_ID
-    , CLIENT_ADD_ITEM, CLIENT_UPDATE_ITEM, CLIENT_DELETE_ITEM, CLIENT_SHOW_ITEM,
-                             ADMIN_ADD_ITEM, ADMIN_UPDATE_ITEM, ADMIN_DELETE_ITEM, ADMIN_SHOW_ITEM )
+insert into crm_admin_role_permission (id, admin_role_id, admin_permission_id
+    , client_permission_add, client_permission_update, client_permission_delete, client_permission_view,
+                                       admin_permission_add, admin_permission_update, admin_permission_delete, admin_permission_view )
 select admin_seq.nextval , (select id from crm_admin_role where title = 'administrator') ,
        crm_permission.PERMISSION_ID, 1,1,1,1,1,1 ,1,1 from crm_permission where not exists
-    (select ADMIN_ROLE_ID, PERMISSION_ID from crm_admin_permission where ADMIN_ROLE_ID = (select id from crm_admin_role where title = 'administrator')  AND crm_permission.PERMISSION_ID = crm_admin_permission.PERMISSION_ID);
+    (select admin_role_id, admin_permission_id from crm_admin_role_permission where admin_role_id = (select id from crm_admin_role where title = 'administrator')  AND crm_permission.PERMISSION_ID = crm_admin_role_permission.admin_permission_id);
